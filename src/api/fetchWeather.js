da95c305f6b89498ce698777955ee17c94601bd7
Kassandra <kassandra@institutsolacroup.com>
2021-05-11 10:54:51 +0200

    Hiding test key

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
+import {API_KEY} from "../apikey";
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'aeb10fd041e31cee8ed5bc24720fbb3f';
 
 export const fetchWeather = async(query) => {
     const {data} = await axios.get(URL, {
